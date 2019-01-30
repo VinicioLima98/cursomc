3311ef68b01b95cc0ccb7c8c0e0cc1581f6ef1af
Vinicio Lima <vinicio-lima@outlook.com>
2019-01-30 17:02:12 -0200

    Banco de Dados H2 e criação automática de database

--- src/main/java/com/viniciolima/cursomc/domain/Categoria.java
+++ src/main/java/com/viniciolima/cursomc/domain/Categoria.java
@@ -2,9 +2,19 @@ package com.viniciolima.cursomc.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+//entidade do JPA
+@Entity
 public class Categoria implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	
@@ -58,9 +68,6 @@ public class Categoria implements Serializable{
 		} else if (!id.equals(other.id))
 			return false;
 		return true;
-	}
-	
-	
-	
+	}	
 
 }
--- src/main/java/com/viniciolima/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/viniciolima/cursomc/resources/CategoriaResource.java
@@ -1,14 +1,10 @@
 package com.viniciolima.cursomc.resources;
 
-
-
 import java.util.ArrayList;
 import java.util.List;
-
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.viniciolima.cursomc.domain.Categoria;
 
 @RestController
